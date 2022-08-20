dcc39bbdc398582d05002f69c0d91ad75769affe
Bartek Pacia <barpac02@gmail.com>
2022-08-21 01:15:13 +0200

    finish part 2

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -1,13 +1,15 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+  var landmark: Landmark
+
   var body: some View {
-    VStack {
-      MapView()
+    ScrollView {
+      MapView(coordinate: landmark.locationCoordinate)
         .ignoresSafeArea(edges: .top)
         .frame(height: 300)
 
-      CircleImage()
+      CircleImage(image: landmark.image)
         .offset(y: -130)
         .padding(.bottom, -130)
 
@@ -30,14 +32,14 @@ struct LandmarkDetail: View {
         Text("Descriptive text goes here.")
       }
       .padding()
-
-      Spacer()
     }
+    .navigationTitle(landmark.name)
+    .navigationBarTitleDisplayMode(.inline)
   }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
   static var previews: some View {
-    LandmarkDetail()
+    LandmarkDetail(landmark: landmarks[0])
   }
 }
--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -5,7 +5,7 @@ struct LandmarkList: View {
     NavigationView {
       List(landmarks) { landmark in
         NavigationLink {
-          LandmarkDetail()
+          LandmarkDetail(landmark: landmark)
         } label: {
           LandmarkRow(landmark: landmark)
         }
@@ -17,6 +17,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
   static var previews: some View {
-    LandmarkList()
+    ForEach(["iPhone SE (3rd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+      LandmarkList()
+        .previewDevice(PreviewDevice(rawValue: deviceName))
+        .previewDisplayName(deviceName)
+    }
   }
 }
