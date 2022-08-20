276ba634f6d6b0c15c426b2e4152d7cc7da35419
Bartek Pacia <barpac02@gmail.com>
2022-08-20 23:18:26 +0200

    finished first part of the tutorial

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -1,21 +1,27 @@
-//
-//  ContentView.swift
-//  Landmarks
-//
-//  Created by Bartek Pacia on 20/08/2022.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    VStack(alignment: .leading) {
+      Text("Turtle Rock")
+        .font(.title)
+
+      HStack {
+        Text("Joshua Tree National Park")
+          .font(.subheadline)
+
+        Spacer()
+
+        Text("California")
+          .font(.subheadline)
+      }
     }
+    .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -1,17 +1,10 @@
-//
-//  LandmarksApp.swift
-//  Landmarks
-//
-//  Created by Bartek Pacia on 20/08/2022.
-//
-
 import SwiftUI
 
 @main
 struct LandmarksApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
