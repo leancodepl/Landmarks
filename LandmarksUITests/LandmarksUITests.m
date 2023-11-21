68cc28b9a98b985c807721fd2887f9789caf11f9
Bartek Pacia <bartek.pacia@leancode.pl>
2023-11-21 15:32:28 +0100

    create test plan

--- LandmarksUITests/LandmarksUITests.m
+++ LandmarksUITests/LandmarksUITests.m
@@ -6,36 +6,39 @@
 
 @implementation LandmarksUITests
 
-
 - (void)testFavorite {
   XCUIApplication *app = [[XCUIApplication alloc] init];
   [app launch];
-  
-  XCUIElementQuery *element = [[app descendantsMatchingType:XCUIElementTypeAny] matchingIdentifier:@"Turtle Rock"];
+
+  XCUIElementQuery *element = [[app descendantsMatchingType:XCUIElementTypeAny]
+      matchingIdentifier:@"Turtle Rock"];
   [[element firstMatch] tap];
 }
 
 - (void)testFavoriteUsingPredicates {
   XCUIApplication *app = [[XCUIApplication alloc] init];
   [app launch];
-  
+
   // Open landmark page
-  NSPredicate *predicate = [NSPredicate predicateWithFormat:@"label = %@", @"Turtle Rock"];
-  XCUIElementQuery *element = [[app descendantsMatchingType:XCUIElementTypeAny] matchingPredicate:predicate];
+  NSPredicate *predicate =
+      [NSPredicate predicateWithFormat:@"label = %@", @"Turtle Rock"];
+  XCUIElementQuery *element = [[app descendantsMatchingType:XCUIElementTypeAny]
+      matchingPredicate:predicate];
   [[element firstMatch] tap];
-  
+
   // Mark landmark as favorite
   predicate = [NSPredicate predicateWithFormat:@"identifier = %@", @"favorite"];
-  element = [[app descendantsMatchingType:XCUIElementTypeAny] matchingPredicate:predicate];
+  element = [[app descendantsMatchingType:XCUIElementTypeAny]
+      matchingPredicate:predicate];
   [[element firstMatch] tap];
-  
+
   // Go to landmarks list
   predicate = [NSPredicate predicateWithFormat:@"label = %@", @"Landmarks"];
-  element = [[app descendantsMatchingType:XCUIElementTypeAny] matchingPredicate:predicate];
+  element = [[app descendantsMatchingType:XCUIElementTypeAny]
+      matchingPredicate:predicate];
   [[element firstMatch] tap];
-  
+
   // Assert that landmark is favorited
-  
 }
 
 @end
